a4518b9f1c770de85a60eb45107dc8a34359951b
Brandon Sauceda <bsauceda@agr.state.ga.us>
2024-05-08 17:15:36 -0400

    8kyu

---
+++ JavaScript/8kyu/if-else-and-ternary-operator.js
@@ -0,0 +1,12 @@
+//INSUTRUCTIONS
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+
+// number of hotdogs	price per unit (cents)
+// n < 5	100
+// n >= 5 and n < 10	95
+// n >= 10	90
+
+//SOLUTION
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n >= 10 ? n * 90 : n * 95;
+}
